23d6f16c0bace586a443b1c9203be0e4595dd922
Muhammed Emre Yılmaz <162283629+Memreyy@users.noreply.github.com>
2024-04-03 11:03:22 +0300

    Akif_solveError.java

--- Errors_solution/solveError.java
+++ Errors_solution/solveError.java
@@ -1,11 +1,14 @@
 public class Multi_D_Array_example {
     public static void main(String[] args) {
         //Declare and initialize
-        int[][] matrix = new int[3][3]
-            {{1,2,3}, 
-             {4,5,6}, 
-            {7,8,9}};
+        int[][] matrix = new int[3][3];
 
+        matrix[0] = new int[] {1,2,3};
+        matrix[1] = new int[] {4,5,6};
+        matrix[2] = new int[] {7,8,9};
+
+
+        
         //access and display
         for(int i= 0; i<matrix.length;i++){
            
