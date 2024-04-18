fe506882aa8a75ddc4ffc8d0a8cf005145ae8007
Umut İsmail Ercivan <162034463+UmutErcivan@users.noreply.github.com>
2024-04-18 12:13:04 +0300

    Add files via upload

---
+++ JavaStuPractices/Örn1.java
@@ -0,0 +1,42 @@
+public class Örn1 {
+    public static void main(String[] args) {
+        int x,y,z,w;
+        x=15;
+        y=5;
+        z=x+y;
+        w=x*y;
+
+        String str1= "Hello";
+        String str2= "World";
+        String result= str1+str2;
+
+        double a=5.5;
+        int b =(int)a;
+
+        boolean isEqual= x==y;
+        boolean isGreater= x>y;
+        boolean result2= isGreater&&isEqual;
+
+        int number=18;
+        number+=6;
+        number-=3;
+
+        printAll(x,y,z,w,b,number,a,result,isEqual,isGreater,result2);
+
+    }
+    public static void printAll(int x,int y,int z,int w,int b,int number,double a,String result,boolean isEqual,boolean isGreater,boolean result2){
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("z="+z);
+        System.out.println("w="+w);
+        System.out.println("b="+b);
+        System.out.println("number="+number);
+        System.out.println("a="+a);
+        System.out.println("result="+result);
+        System.out.println("isEqual="+isEqual);
+        System.out.println("isGreater="+isGreater);
+        System.out.println("result2="+result2);
+    }
+
+
+}
